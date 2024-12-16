e51e8e55544420b3f1b873010475a38dbed971bd
Resty Pabello <choloprince27@gmail.com>
2024-12-16 17:06:29 +0800

    Add form validation and adjust the password length

---
+++ app/schemas/user_schema.py
@@ -0,0 +1,40 @@
+from marshmallow import Schema, fields, validate, ValidationError
+
+def non_empty_string(value):
+    if not value.strip():
+        raise ValidationError("Name cannot be empty or just whitespace.")
+    
+def validate_password(password):
+    if len(password) < 8:
+        raise ValidationError("Password must be at least 8 characters long.")
+    if not any(char.isdigit() for char in password):
+        raise ValidationError("Password must contain at least one number.")
+    if not any(char.isalpha() for char in password):
+        raise ValidationError("Password must contain at least one letter.")
+    if not any(char in "!@#$%^&*()-_+=" for char in password):
+        raise ValidationError("Password must contain at least one special character.")
+
+class UserSchema(Schema):
+    email = fields.Email(
+        required=True, 
+        error_messages={
+            "required": "Email is required.", 
+            "invalid": "Invalid email address."
+        }
+    )
+    role_id = fields.Integer(required=True)
+    password = fields.String(
+        required=True,
+        validate=[validate_password, validate.Length(max=128)],
+        error_messages={
+            "required": "Password is required."
+        }
+    )
+    confirm_password = fields.String(
+        required=True,
+        error_messages={"required": "Password confirmation is required."}
+    )
+    def validate_passwords_match(self, value):
+        if value != self.context.get('password'):
+            raise ValidationError("Passwords do not match.")
+    
